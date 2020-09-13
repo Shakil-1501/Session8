0847b3bcc111d241d3bb47316b8e9f61046b2ed3
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-09-13 22:15:53 +0530

    Add files via upload

--- test_session8.py
+++ test_session8.py
@@ -30,3 +30,28 @@ def test_function_calling_single_dict():
     assert k == {'add': 3, 'div': 1, 'mul': 1}
 
 
+def test_function_calling_sep_dict():
+    c=session8.counter2(session8.add,session8.dicA)
+    c(3,6)
+    c(4,5)
+    l=session8.counter2(session8.mul,session8.dicA)
+    l(3,2)
+    l(4,5)
+    x=session8.counter2(session8.div,session8.dicA)
+    x(12,2)
+    x(10,5)
+    c=session8.counter2(session8.add,session8.dicB)
+    c(2,4)
+    c(3,7)
+    c(2,3)
+    l=session8.counter2(session8.mul,session8.dicB)
+    l(3,2)
+    x=session8.counter2(session8.div,session8.dicB)
+    x(12,2)
+    x(10,5)
+    assert session8.dicA == {'add': 2, 'div': 2, 'mul': 2} and session8.dicB == {'add': 3, 'div': 2, 'mul': 1}
+
+
+
+
+
